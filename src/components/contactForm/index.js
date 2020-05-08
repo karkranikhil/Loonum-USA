e708af7964a90820a36c77eff34e2d54aad116a0
nikhil.karkra@rmit.edu.au <nikhil.karkra@rmit.edu.au>
2020-05-08 12:19:12 +1000

    netlify forms

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -1,13 +1,16 @@
 
-import React, { useState } from "react"
-import addToMailchimp from 'gatsby-plugin-mailchimp'
+import React, { useState, useRef } from "react"
+// import addToMailchimp from 'gatsby-plugin-mailchimp'
+import axios from "axios"
+import * as qs from "query-string"
 import contactFormStyle from './contactForm.module.scss'
 const PRIVACY_TEXT = 'By checking this box and clicking submit, I hereby consent that Loonum sends me via email marketing related information with regard to products and services of Loonum. I understand that, in order to opt-out from receiving such emails in the future, I can use the opt-out mechanism that is described in each such email. Privacy Policy | GDPR'
 const FORM_DATA = {
-    FNAME: '',
-    LNAME: '',
+    title: 'Request for Get in touch',
+    first_name: '',
+    last_name: '',
     company: '',
-    title: '',
+    job_title: '',
     email: '',
     phone: '',
     country: '',
@@ -17,23 +20,42 @@ const FORM_DATA = {
 }
 const ContactForm = ({ color }) => {
     const [formData, setFormData] = useState(FORM_DATA)
-    const _handleSubmit = e => {
-        e.preventDefault();
-        let email = formData.email
-        let listFields = {
-            ...formData
+    const formRef = useRef(null)
+    // const _handleSubmit = e => {
+    //     e.preventDefault();
+    //     let email = formData.email
+    //     let listFields = {
+    //         ...formData
+    //     }
+    //     addToMailchimp(email, listFields) // listFields are optional if you are only capturing the email address.
+    //         .then(data => {
+    //             // I recommend setting data to React state
+    //             // but you can do whatever you want (including ignoring this `then()` altogether)
+    //             console.log(data)
+    //         })
+    //         .catch(() => {
+    //             // unnecessary because Mailchimp only ever
+    //             // returns a 200 status code
+    //             // see below for how to handle errors
+    //         })
+    // }
+    const handleSubmit = (event) => {
+        // Do not submit form via HTTP, since we're doing that via XHR request.
+        event.preventDefault()
+        const axiosOptions = {
+            url: window.location.pathname,
+            method: "post",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            data: qs.stringify(formData),
         }
-        addToMailchimp(email, listFields) // listFields are optional if you are only capturing the email address.
-            .then(data => {
-                // I recommend setting data to React state
-                // but you can do whatever you want (including ignoring this `then()` altogether)
-                console.log(data)
+        axios(axiosOptions)
+            .then(response => {
+                this.formRef.current.reset()
+                console.log("success", response)
             })
-            .catch(() => {
-                // unnecessary because Mailchimp only ever
-                // returns a 200 status code
-                // see below for how to handle errors
-            })
+            .catch(err =>
+                console.log("error")
+            )
     }
     const formHandler = e => {
         const { name, value } = e.target
@@ -52,14 +74,14 @@ const ContactForm = ({ color }) => {
                         </div>
                     </div>
                     <div className="col-12 col-sm-6 mt-3 mt-sm-none">
-                        <form netlify-honeypot="bot-field" data-netlify="true">
+                        <form ref={formRef} name="Contact Form" method="POST" data-netlify="true" onSubmit={handleSubmit}>
                             <input type="hidden" name="bot-field" />
                             <div className="row">
                                 <div className="col-12 col-sm-6 mb-3">
-                                    <input type="text" className="form-control" id="FNAME" placeholder="First Name" name="FNAME" value={formData.FNAME} onChange={formHandler} />
+                                    <input type="text" className="form-control" id="first_name" placeholder="First Name" name="first_name" value={formData.first_name} onChange={formHandler} />
                                 </div>
                                 <div className="col-12 col-sm-6 mb-3">
-                                    <input type="text" className="form-control" id="LNAME" placeholder="Last Name" name="LNAME" value={formData.LNAME} onChange={formHandler} />
+                                    <input type="text" className="form-control" id="last_name" placeholder="Last Name" name="last_name" value={formData.last_name} onChange={formHandler} />
                                 </div>
                             </div>
                             <div className="row">
@@ -67,7 +89,7 @@ const ContactForm = ({ color }) => {
                                     <input type="text" className="form-control" id="company" placeholder="Company" name="company" value={formData.company} onChange={formHandler} />
                                 </div>
                                 <div className="col-12 col-sm-6 mb-3">
-                                    <input type="text" className="form-control" id="title" placeholder="Job title" name="title" value={formData.title} onChange={formHandler} />
+                                    <input type="text" className="form-control" id="job_title" placeholder="Job title" name="job_title" value={formData.job_title} onChange={formHandler} />
                                 </div>
                             </div>
                             <div className="row">
@@ -95,7 +117,7 @@ const ContactForm = ({ color }) => {
                             <div className="row">
                                 <div className={`col ${contactFormStyle.privacy_text}`}>
                                     <div className="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" className="custom-control-input" id="customCheck" name="privacyAccepted" value={formData.privacyAccepted} onChange={formHandler} />
+                                        <input type="checkbox" className="custom-control-input" id="customCheck" name="privacyAccepted" checked={formData.privacyAccepted} onChange={formHandler} />
                                         <label className="custom-control-label" htmlFor="customCheck">{PRIVACY_TEXT}</label>
                                     </div>
                                 </div>
